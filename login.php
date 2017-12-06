6aa9cdf5430229f5fe3ccaa544ae9c21e28b6ca8
user <jacky.cheung@colorado.edu>
2017-12-06 15:03:43 -0700

    commiting in order to pull

--- login.php
+++ login.php
@@ -21,60 +21,13 @@ $queryP = mysqli_query($connection, "SELECT password FROM login WHERE password =
 $u = mysqli_fetch_array($queryU);
 $p = mysqli_fetch_array($queryP);
 
-if($u["username"] == $user and $u["password"] == $pass){	
+if($u["username"] == $user and $p["password"] == $pass){	
 	echo "Login Successful";
 }
 else{
 	echo "Username or password is incorrect";
 }
 mysqli_close($connection);
-/*
-if(mysqli_connect_errno())
-{ 
-echo "<h4>Failed to connect to MySQL: </h4>".mysqli_connect_error(); 
-} 
-else 
-{ 
-echo "<h4>Successfully connected to MySQL: </h4>"; 
-} 
-
-
-function NewUser()
-{	
-	
-	$connect = @mysqli_connect (localhost, root, Jc2011368, login);#You have to put your password in between root, and loginAccounts for this to work
-	$email = $_POST['email'];
-	$password =  $_POST['password'];
-	$query = "INSERT INTO user (email,password) VALUES ('$email','$password')";
-	$data = mysqli_query ($connect, $query); #or die(mysqli_error());
-	if($data)
-	{
-	echo "YOUR REGISTRATION IS COMPLETED...";
-	}
-}
-#This function does not work yet so I skipped calling it in the if statement below it and just called NewUser() instead -Joel
-function SignUp()
-{
-if(!empty($_POST['user']))   //checking the 'user' name which is from Sign-Up.html, is it empty or have some text
-{
-	$query = mysqli_query("SELECT * FROM user WHERE email = '$_POST[email]' AND pass = '$_POST[password]'") or die(mysql_error());
-
-	if(!$row = mysql_fetch_array($query) or die(mysqli_error()))
-	{
-		NewUser();
-	}
-	else
-	{
-		echo "SORRY...YOU ARE ALREADY REGISTERED USER...";
-	}
-}
-}
-if(isset($_POST['submit']))
-{
-	echo "<h4>Successfully received submit: </h4>";
-	NewUser();
-}
-*/
 ?>
 
 </body>
--- signup.php
+++ signup.php
@@ -35,34 +35,6 @@ else
 echo "Passwords don't match";
 }
 mysqli_close($connection);
-/*
-
-function NewUser() { 
-	$name = $_POST['name']; 
-	$user = $_POST['user']; 
-	$email = $_POST['email']; 
-	$pass = $_POST['pass']; 
-	$query = "INSERT INTO login (name,email,user,pass) VALUES ('$name','$email','$user','$pass')"; 
-	$data = mysql_query ($query)or die(mysql_error()); 
-	if($data) { 
-	echo "YOUR REGISTRATION IS COMPLETED..."; 
-	} 
-} 
-function SignUp() { 
-if(!empty($_POST['user'])){ 
-$query = mysql_query("SELECT * FROM login WHERE user = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error()); 
-if(!$row = mysql_fetch_array($query) or die(mysql_error())) {
-	NewUser(); 
-} 
-else {
-echo "SORRY...YOU ARE ALREADY REGISTERED USER..."; 
-} 
-} 
-} 
-if(isset($_POST['submit'])) {
- SignUp(); 
-}
-*/
 ?>
 
 </body>
