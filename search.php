e1dee9cff4d669e17c1fe14372c4f8983150b467
Daniel Rafael <dara6996@colorado.edu>
2017-12-10 16:35:55 -0700

    Got the recipe page working just search and page .php need formatting

--- search.php
+++ search.php
@@ -1,7 +1,6 @@
-
 <?php
 //include "mainpage.php";
-$connection = @mysqli_connect (localhost, root, 'Dht19960313!', login);
+$connection = @mysqli_connect (localhost, root, danraf12, FoodData);
 if(mysqli_connect_errno())
 {
 echo "<h4>Failed to connect to MySQL:
@@ -9,21 +8,16 @@ echo "<h4>Failed to connect to MySQL:
 }
 
 $search = $_REQUEST['food'];
-$queryN = mysqli_query($connection, "SELECT *  FROM food WHERE name = '$search'");
+$queryN = mysqli_query($connection, "SELECT *  FROM food WHERE name LIKE '%$search%'");
 
 while($row =  mysqli_fetch_array($queryN,MYSQLI_NUM))
-{ 	$i =1;
-	$j =1;
-	echo "Name: ".$row[0]."<br>";
-	while($i <31)
-	{
-		echo $j.". ".$row[$i]." ".$row[$i+1]."<br>";
-		$j++;
-		$i++;
-		$i++;
-	}
-	echo "Prep: ". $row[32]."<br>";
-	echo "Description: ". $row[33]."<br>";
+{
+echo "<tr>";
+echo "<td>Name:</td><td> <form action = page.php> <input type=submit name=submit  value='$row[0]'></form></td>";
+//echo "Name: <a href=page.php>".$row[0]."</a><br>";
+//echo "Name: ".$row[0]."<br>";
+echo "Prep: ". $row[32]."<br>";
+echo "</tr>";
 }
 mysqli_close($connection);
 ?>
